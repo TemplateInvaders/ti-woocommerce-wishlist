9105103e121680bebe0a13444168a606177e4764
Doozy <tiagohok@gmail.com>
2017-11-21 19:47:10 +0700

    version bumped to 1.5.11

--- ti-woocommerce-wishlist.php
+++ ti-woocommerce-wishlist.php
@@ -4,7 +4,7 @@
  * Plugin Name:       WooCommerce Wishlist Plugin
  * Plugin URI:        https://wordpress.org/plugins/ti-woocommerce-wishlist/
  * Description:       Wishlist functionality for your WooCommerce store.
- * Version:           1.5.10
+ * Version:           1.5.11
  * Requires at least: 4.5
  * Tested up to: 4.9
  * WC requires at least: 2.6
@@ -41,7 +41,7 @@ if ( ! defined( 'TINVWL_DOMAIN' ) ) {
 }
 
 if ( ! defined( 'TINVWL_FVERSION' ) ) {
-	define( 'TINVWL_FVERSION', '1.5.10' );
+	define( 'TINVWL_FVERSION', '1.5.11' );
 }
 
 if ( ! defined( 'TINVWL_LOAD_FREE' ) ) {

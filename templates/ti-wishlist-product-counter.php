e5883dbb4d16ce94c9c865e412e2e52048c4546d
Auer Serg <auerserg@gmail.com>
2017-12-14 14:11:21 +0200

    Change the counter link to the account page

--- templates/ti-wishlist-product-counter.php
+++ templates/ti-wishlist-product-counter.php
@@ -14,17 +14,9 @@ if ( $icon_class && 'custom' === $icon && ! empty( $icon_upload )  ) {
 	$text = sprintf( '<img src="%s" /> %s', esc_url( $icon_upload ), $text );
 }
 ?>
-<?php if ( tinv_get_option( 'general', 'require_login' ) ) : ?>
-	<div class="wishlist_products_counter<?php echo ' ' . $icon_class . ' ' . $icon_style . ( empty( $text ) ? ' no-txt' : '' ) . ( 0 < $counter ? ' wishlist-counter-with-products' : '' ); // WPCS: xss ok. ?>">
-<?php else : ?>
-	<a href="<?php echo esc_url( tinv_url_wishlist_default() ); ?>" class="wishlist_products_counter<?php echo ' ' . $icon_class . ' ' . $icon_style . ( empty( $text ) ? ' no-txt' : '' ) . ( 0 < $counter ? ' wishlist-counter-with-products' : '' ); // WPCS: xss ok. ?>">
-<?php endif; ?>
+<a href="<?php echo esc_url( tinv_get_option( 'general', 'require_login' ) ? wc_get_page_permalink( 'myaccount' ) : tinv_url_wishlist_default() ); ?>" class="wishlist_products_counter<?php echo ' ' . $icon_class . ' ' . $icon_style . ( empty( $text ) ? ' no-txt' : '' ) . ( 0 < $counter ? ' wishlist-counter-with-products' : '' ); // WPCS: xss ok. ?>">
 	<span class="wishlist_products_counter_text"><?php echo $text; // WPCS: xss ok. ?></span>
 	<?php if ( $show_counter ) : ?>
 		<span class="wishlist_products_counter_number"></span>
 	<?php endif; ?>
-<?php if ( tinv_get_option( 'general', 'require_login' ) ) : ?>
-</div>
-<?php else : ?>
 </a>
-<?php endif; ?>

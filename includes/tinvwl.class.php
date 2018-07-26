277ea049e91842255c29dd706003ada7a84bcf8a
Doozy <tiagohok@gmail.com>
2018-07-26 13:53:43 +0700

    added `tinvwl-load_frontend` filter to conditionally load wishlist frontend from 3rd party plugins.

--- includes/tinvwl.class.php
+++ includes/tinvwl.class.php
@@ -46,15 +46,19 @@ class TInvWL {
 	 * Created admin and public class
 	 */
 	function __construct() {
-		$this->_name			 = TINVWL_PREFIX;
-		$this->_version			 = TINVWL_FVERSION;
+		$this->_name    = TINVWL_PREFIX;
+		$this->_version = TINVWL_FVERSION;
 
 		$this->set_locale();
 		$this->maybe_update();
 		$this->load_function();
 		$this->define_hooks();
-		$this->object_admin	 = new TInvWL_Admin_TInvWL( $this->_name, $this->_version );
-		$this->object_public = TInvWL_Public_TInvWL::instance( $this->_name, $this->_version );
+		$this->object_admin = new TInvWL_Admin_TInvWL( $this->_name, $this->_version );
+
+		// Allow to disable wishlist for frontend conditionally. Must be hooked on 'plugins_loaded' action.
+		if ( apply_filters( 'tinvwl-load_frontend', true ) ) {
+			$this->object_public = TInvWL_Public_TInvWL::instance( $this->_name, $this->_version );
+		}
 	}
 
 	/**
@@ -64,30 +68,32 @@ class TInvWL {
 		if ( is_null( get_option( $this->_name . '_db_ver', null ) ) ) {
 			TInvWL_Activator::activate();
 		}
-		$object = null;
+
 		TInvWL_View::_init( $this->_name, $this->_version );
 		TInvWL_Form::_init( $this->_name );
 
 		if ( is_admin() ) {
 			new TInvWL_WizardSetup( $this->_name, $this->_version );
-			$object = $this->object_admin;
+			$this->object_admin->load_function();
 		} else {
-			$object = $this->object_public;
+			// Allow to disable wishlist for frontend conditionally. Must be hooked on 'plugins_loaded' action.
+			if ( apply_filters( 'tinvwl-load_frontend', true ) ) {
+				$this->object_public->load_function();
+			}
 		}
-		$object->load_function();
 	}
 
 	/**
 	 * Set localization
 	 */
 	private function set_locale() {
-		$locale	 = apply_filters( 'plugin_locale', get_locale(), TINVWL_DOMAIN );
-		$mofile	 = sprintf( '%1$s-%2$s.mo', TINVWL_DOMAIN, $locale );
+		$locale  = apply_filters( 'plugin_locale', get_locale(), TINVWL_DOMAIN );
+		$mofile  = sprintf( '%1$s-%2$s.mo', TINVWL_DOMAIN, $locale );
 		$mofiles = array();
 
-		$mofiles[]	 = WP_LANG_DIR . DIRECTORY_SEPARATOR . basename( TINVWL_PATH ) . DIRECTORY_SEPARATOR . $mofile;
-		$mofiles[]	 = WP_LANG_DIR . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR . $mofile;
-		$mofiles[]	 = TINVWL_PATH . 'languages' . DIRECTORY_SEPARATOR . $mofile;
+		$mofiles[] = WP_LANG_DIR . DIRECTORY_SEPARATOR . basename( TINVWL_PATH ) . DIRECTORY_SEPARATOR . $mofile;
+		$mofiles[] = WP_LANG_DIR . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR . $mofile;
+		$mofiles[] = TINVWL_PATH . 'languages' . DIRECTORY_SEPARATOR . $mofile;
 		foreach ( $mofiles as $mofile ) {
 			if ( file_exists( $mofile ) && load_textdomain( TINVWL_DOMAIN, $mofile ) ) {
 				return;
@@ -101,7 +107,10 @@ class TInvWL {
 	 * Define hooks
 	 */
 	function define_hooks() {
-	    add_filter( 'plugin_action_links_' . plugin_basename( TINVWL_PATH . 'ti-woocommerce-wishlist.php' ), array( $this, 'action_links' ) );
+		add_filter( 'plugin_action_links_' . plugin_basename( TINVWL_PATH . 'ti-woocommerce-wishlist.php' ), array(
+			$this,
+			'action_links'
+		) );
 		add_action( 'after_setup_theme', 'tinvwl_set_utm', 100 );
 	}
 
@@ -138,12 +147,13 @@ class TInvWL {
 	 * @access public
 	 *
 	 * @param mixed $links Links.
+	 *
 	 * @return array
 	 */
 	public function action_links( $links ) {
-		$plugin_links[]	 = '<a href="' . admin_url( 'admin.php?page=tinvwl' ) . '">' . __( 'Settings', 'ti-woocommerce-wishlist' ) . '</a>';
-		$plugin_links[]	 = '<a target="_blank" href="https://templateinvaders.com/product/ti-woocommerce-wishlist-wordpress-plugin/?utm_source=' . TINVWL_UTM_SOURCE . '&utm_campaign=' . TINVWL_UTM_CAMPAIGN . '&utm_medium=' . TINVWL_UTM_MEDIUM . '&utm_content=action_link&partner=' . TINVWL_UTM_SOURCE . '" style="color:#46b450;font-weight:700;">' . __( 'Premium Version', 'ti-woocommerce-wishlist' ) . '</a>';
-		$plugin_links[]	 = '<a target="_blank" href="https://woocommercewishlist.com/preview/?utm_source=' . TINVWL_UTM_SOURCE . '&utm_campaign=' . TINVWL_UTM_CAMPAIGN . '&utm_medium=' . TINVWL_UTM_MEDIUM . '&utm_content=action_link&partner=' . TINVWL_UTM_SOURCE . '"  style="color:#515151">' . __( 'Live Demo', 'ti-woocommerce-wishlist' ) . '</a>';
+		$plugin_links[] = '<a href="' . admin_url( 'admin.php?page=tinvwl' ) . '">' . __( 'Settings', 'ti-woocommerce-wishlist' ) . '</a>';
+		$plugin_links[] = '<a target="_blank" href="https://templateinvaders.com/product/ti-woocommerce-wishlist-wordpress-plugin/?utm_source=' . TINVWL_UTM_SOURCE . '&utm_campaign=' . TINVWL_UTM_CAMPAIGN . '&utm_medium=' . TINVWL_UTM_MEDIUM . '&utm_content=action_link&partner=' . TINVWL_UTM_SOURCE . '" style="color:#46b450;font-weight:700;">' . __( 'Premium Version', 'ti-woocommerce-wishlist' ) . '</a>';
+		$plugin_links[] = '<a target="_blank" href="https://woocommercewishlist.com/preview/?utm_source=' . TINVWL_UTM_SOURCE . '&utm_campaign=' . TINVWL_UTM_CAMPAIGN . '&utm_medium=' . TINVWL_UTM_MEDIUM . '&utm_content=action_link&partner=' . TINVWL_UTM_SOURCE . '"  style="color:#515151">' . __( 'Live Demo', 'ti-woocommerce-wishlist' ) . '</a>';
 
 		return array_merge( $links, $plugin_links );
 	}

cbaa7a599b159f6fe8e38fc0308d171ff21336ba
Doozy <tiagohok@gmail.com>
2018-08-16 12:37:59 +0700

    version bumped to 1.8.12

--- assets/css/public.min.css
+++ assets/css/public.min.css
@@ -1,4 +1,4 @@
-/*! WooCommerce Wishlist Plugin - version 1.8.11
+/*! WooCommerce Wishlist Plugin - version 1.8.12
  * https://wordpress.org/plugins/ti-woocommerce-wishlist/
  * Copyright (c) 2018;
  * Licensed GPLv2+ */

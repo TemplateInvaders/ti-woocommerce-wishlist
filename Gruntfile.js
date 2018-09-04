6ddb109b498dc8964c8044ad2548ff0837bbede5
Doozy <tiagohok@gmail.com>
2018-09-04 20:40:22 +0700

    code quality fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
-'use strict';
+"use strict";
 
-const sass = require('node-sass');
+const sass = require("node-sass");
 
 module.exports = function (grunt) {
 
 	// require it at the top and pass in the grunt instance
-	require('time-grunt')(grunt);
+	require("time-grunt")(grunt);
 
 	// Load all Grunt tasks
-	require('jit-grunt')(grunt, {
-		makepot: 'grunt-wp-i18n'
+	require("jit-grunt")(grunt, {
+		makepot: "grunt-wp-i18n"
 	});
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 
 		// Concat js.
 		concat: {
 			dist: {
 				files: {
-					'assets/js/public.js': ['assets/js/button.js', 'assets/js/table.js', 'assets/js/misc.js']
+					"assets/js/public.js": ["assets/js/button.js", "assets/js/table.js", "assets/js/misc.js"]
 				}
 			}
 		},
@@ -29,8 +29,8 @@ module.exports = function (grunt) {
 		uglify: {
 			dist: {
 				files: {
-					'assets/js/public.min.js': 'assets/js/public.js',
-					'assets/js/admin.min.js': 'assets/js/admin.js'
+					"assets/js/public.min.js": "assets/js/public.js",
+					"assets/js/admin.min.js": "assets/js/admin.js"
 				}
 			}
 		},
@@ -45,12 +45,12 @@ module.exports = function (grunt) {
 				},
 
 				files: {
-					'assets/css/admin.min.css': 'assets/css/admin.css',
-					'assets/css/admin-form.min.css': 'assets/css/admin-form.css',
-					'assets/css/admin-setup.min.css': 'assets/css/admin-setup.css',
-					'assets/css/bootstrap.min.css': 'assets/css/bootstrap.css',
-					'assets/css/public.min.css': 'assets/css/public.css',
-					'assets/css/theme.min.css': 'assets/css/theme.css'
+					"assets/css/admin.min.css": "assets/css/admin.css",
+					"assets/css/admin-form.min.css": "assets/css/admin-form.css",
+					"assets/css/admin-setup.min.css": "assets/css/admin-setup.css",
+					"assets/css/bootstrap.min.css": "assets/css/bootstrap.css",
+					"assets/css/public.min.css": "assets/css/public.css",
+					"assets/css/theme.min.css": "assets/css/theme.css"
 				}
 			}
 		},
@@ -61,16 +61,16 @@ module.exports = function (grunt) {
 			dist: {
 				options: {
 					browsers: [
-						'last 8 versions', 'ie 8', 'ie 9'
+						"last 8 versions", "ie 8", "ie 9"
 					]
 				},
 
 				files: {
-					'assets/css/public.css': 'assets/css/public.css',
-					'assets/css/theme.css': 'assets/css/theme.css',
-					'assets/css/admin.css': 'assets/css/admin.css',
-					'assets/css/admin-form.css': 'assets/css/admin-form.css',
-					'assets/css/admin-setup.css': 'assets/css/admin-setup.css'
+					"assets/css/public.css": "assets/css/public.css",
+					"assets/css/theme.css": "assets/css/theme.css",
+					"assets/css/admin.css": "assets/css/admin.css",
+					"assets/css/admin-form.css": "assets/css/admin-form.css",
+					"assets/css/admin-setup.css": "assets/css/admin-setup.css"
 				}
 			}
 		},
@@ -80,14 +80,14 @@ module.exports = function (grunt) {
 		sass: {
 			dist: {
 				options: {
-					outputStyle: 'expanded',
+					outputStyle: "expanded",
 					implementation: sass,
 					sourceMap: false,
 				},
 				files: {
-					'assets/css/admin.css': 'sass/admin.scss',
-					'assets/css/theme.css': 'sass/theme.scss',
-					'assets/css/public.css': 'sass/public.scss',
+					"assets/css/admin.css": "sass/admin.scss",
+					"assets/css/theme.css": "sass/theme.scss",
+					"assets/css/public.css": "sass/public.scss",
 
 				}
 			},
@@ -97,27 +97,27 @@ module.exports = function (grunt) {
 		makepot: {
 			target: {
 				options: {
-					domainPath: '/languages/', // Where to save the POT file.
+					domainPath: "/languages/", // Where to save the POT file.
 					exclude: [ // Exlude folder.
-						'.idea/.*',
-						'assets/.*',
-						'node_modules/.*',
-						'build/.*'
+						".idea/.*",
+						"assets/.*",
+						"node_modules/.*",
+						"build/.*"
 					],
-					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
-					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
+					potFilename: "<%= pkg.name %>.pot", // Name of the POT file.
+					type: "wp-plugin", // Type of project (wp-plugin or wp-theme).
 					updateTimestamp: false, // Whether the POT-Creation-Date should be updated without other changes.
 					processPot: function (pot, options) {
-						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
-						pot.headers['last-translator'] = 'TemplateInvaders\n';
-						pot.headers['language-team'] = 'TemplateInvaders\n';
-						pot.headers['x-poedit-basepath'] = '..\n';
-						pot.headers['x-poedit-language'] = 'English\n';
-						pot.headers['x-poedit-country'] = 'UNITED STATES\n';
-						pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
-						pot.headers['x-poedit-searchpath-0'] = '.\n';
-						pot.headers['x-poedit-keywordslist'] = '_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
-						pot.headers['x-textdomain-support'] = 'yes\n';
+						pot.headers["plural-forms"] = "nplurals=2; plural=n != 1;";
+						pot.headers["last-translator"] = "TemplateInvaders\n";
+						pot.headers["language-team"] = "TemplateInvaders\n";
+						pot.headers["x-poedit-basepath"] = "..\n";
+						pot.headers["x-poedit-language"] = "English\n";
+						pot.headers["x-poedit-country"] = "UNITED STATES\n";
+						pot.headers["x-poedit-sourcecharset"] = "utf-8\n";
+						pot.headers["x-poedit-searchpath-0"] = ".\n";
+						pot.headers["x-poedit-keywordslist"] = "_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n";
+						pot.headers["x-textdomain-support"] = "yes\n";
 						return pot;
 					}
 				}
@@ -127,21 +127,21 @@ module.exports = function (grunt) {
 		header: {
 			dist: {
 				options: {
-					text: '/*! <%= pkg.title %> - version <%= pkg.version %>\n' +
-						' * <%= pkg.homepage %>\n' +
-						' * Copyright (c) <%= grunt.template.today("yyyy") %>;\n' +
-						' * Licensed GPLv2+' +
-						' */\n'
+					text: "/*! <%= pkg.title %> - version <%= pkg.version %>\n" +
+						" * <%= pkg.homepage %>\n" +
+						" * Copyright (c) <%= grunt.template.today('yyyy') %>;\n" +
+						" * Licensed GPLv2+" +
+						" */\n"
 				},
 				files:
 					{
-						'assets/css/admin.min.css': 'assets/css/admin.min.css',
-						'assets/css/admin-form.min.css': 'assets/css/admin-form.min.css',
-						'assets/css/admin-setup.min.css': 'assets/css/admin-setup.min.css',
-						'assets/css/public.min.css': 'assets/css/public.min.css',
-						'assets/css/theme.min.css': 'assets/css/theme.min.css',
-						'assets/js/public.min.js': 'assets/js/public.min.js',
-						'assets/js/admin.min.js': 'assets/js/admin.min.js'
+						"assets/css/admin.min.css": "assets/css/admin.min.css",
+						"assets/css/admin-form.min.css": "assets/css/admin-form.min.css",
+						"assets/css/admin-setup.min.css": "assets/css/admin-setup.min.css",
+						"assets/css/public.min.css": "assets/css/public.min.css",
+						"assets/css/theme.min.css": "assets/css/theme.min.css",
+						"assets/js/public.min.js": "assets/js/public.min.js",
+						"assets/js/admin.min.js": "assets/js/admin.min.js"
 					}
 
 			}
@@ -152,34 +152,34 @@ module.exports = function (grunt) {
 			build: {
 				expand: true,
 				src: [
-					'**',
-					'!node_modules/**',
-					'!bower_components/**',
-					'!build/**',
-					'!sass/**',
-					'!.git/**',
-					'!Gruntfile.js',
-					'!package.json',
-					'!desktop.ini',
-					'!prepros.cfg',
-					'!CONTRIBUTING.md',
-					'!readme.md',
-					'!.csscomb.json',
-					'!.tern-project',
-					'!.gitignore',
-					'!.jshintrc',
-					'!.DS_Store',
-					'!*.map',
-					'!**/*.map',
-					'!**/Gruntfile.js',
-					'!**/package.json',
-					'!**/package-lock.json',
-					'!**/*~',
-					'!assets/js/button.js',
-					'!assets/js/table.js',
-					'!assets/js/misc.js'
+					"**",
+					"!node_modules/**",
+					"!bower_components/**",
+					"!build/**",
+					"!sass/**",
+					"!.git/**",
+					"!Gruntfile.js",
+					"!package.json",
+					"!desktop.ini",
+					"!prepros.cfg",
+					"!CONTRIBUTING.md",
+					"!readme.md",
+					"!.csscomb.json",
+					"!.tern-project",
+					"!.gitignore",
+					"!.jshintrc",
+					"!.DS_Store",
+					"!*.map",
+					"!**/*.map",
+					"!**/Gruntfile.js",
+					"!**/package.json",
+					"!**/package-lock.json",
+					"!**/*~",
+					"!assets/js/button.js",
+					"!assets/js/table.js",
+					"!assets/js/misc.js"
 				],
-				dest: 'build/<%= pkg.name %>/'
+				dest: "build/<%= pkg.name %>/"
 			}
 		},
 
@@ -187,53 +187,53 @@ module.exports = function (grunt) {
 		compress: {
 			build: {
 				options: {
-					mode: 'zip',
-					archive: './build/<%= pkg.name %>.zip'
+					mode: "zip",
+					archive: "./build/<%= pkg.name %>.zip"
 				},
 				expand: true,
-				cwd: 'build/<%= pkg.name %>/',
-				src: ['**/*'],
-				dest: '<%= pkg.name %>/'
+				cwd: "build/<%= pkg.name %>/",
+				src: ["**/*"],
+				dest: "<%= pkg.name %>/"
 			}
 		},
 
 		// Clean up build directory
 		clean: {
 			build: [
-				'build/<%= pkg.name %>',
-				'build/<%= pkg.name %>.zip'
+				"build/<%= pkg.name %>",
+				"build/<%= pkg.name %>.zip"
 			],
 			after: [
-				'build/<%= pkg.name %>',
+				"build/<%= pkg.name %>",
 			]
 		},
 
 
 		checktextdomain: {
 			options: {
-				text_domain: 'ti-woocommerce-wishlist',
+				text_domain: "ti-woocommerce-wishlist",
 				keywords: [
-					'__:1,2d',
-					'_e:1,2d',
-					'_x:1,2c,3d',
-					'esc_html__:1,2d',
-					'esc_html_e:1,2d',
-					'esc_html_x:1,2c,3d',
-					'esc_attr__:1,2d',
-					'esc_attr_e:1,2d',
-					'esc_attr_x:1,2c,3d',
-					'_ex:1,2c,3d',
-					'_n:1,2,4d',
-					'_nx:1,2,4c,5d',
-					'_n_noop:1,2,3d',
-					'_nx_noop:1,2,3c,4d'
+					"__:1,2d",
+					"_e:1,2d",
+					"_x:1,2c,3d",
+					"esc_html__:1,2d",
+					"esc_html_e:1,2d",
+					"esc_html_x:1,2c,3d",
+					"esc_attr__:1,2d",
+					"esc_attr_e:1,2d",
+					"esc_attr_x:1,2c,3d",
+					"_ex:1,2c,3d",
+					"_n:1,2,4d",
+					"_nx:1,2,4c,5d",
+					"_n_noop:1,2,3d",
+					"_nx_noop:1,2,3c,4d"
 				]
 			},
 			files: {
 				src: [
-					'**/*.php', // Include all files
-					'!node_modules/**', // Exclude node_modules/
-					'!build/**', // Exclude build/
+					"**/*.php", // Include all files
+					"!node_modules/**", // Exclude node_modules/
+					"!build/**", // Exclude build/
 				],
 				expand: true
 			}
@@ -241,7 +241,7 @@ module.exports = function (grunt) {
 		wp_readme_to_markdown: {
 			your_target: {
 				files: {
-					'readme.md': 'readme.txt'
+					"readme.md": "readme.txt"
 				},
 			},
 		},
@@ -249,24 +249,24 @@ module.exports = function (grunt) {
 	});
 
 
-	grunt.registerTask('default', [
-		'concat',
-		'uglify',
-		'sass',
-		'autoprefixer',
-		'cssmin',
-		'header',
-		'checktextdomain',
+	grunt.registerTask("default", [
+		"concat",
+		"uglify",
+		"sass",
+		"autoprefixer",
+		"cssmin",
+		"header",
+		"checktextdomain",
 
 	]);
 
-	grunt.registerTask('package', [
-		'wp_readme_to_markdown',
-		'makepot',
-		'clean:build',
-		'copy',
-		'compress',
-		'clean:after',
+	grunt.registerTask("package", [
+		"wp_readme_to_markdown",
+		"makepot",
+		"clean:build",
+		"copy",
+		"compress",
+		"clean:after",
 	]);
 
 

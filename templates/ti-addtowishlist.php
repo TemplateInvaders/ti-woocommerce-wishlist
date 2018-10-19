e5002b51002e89bf1ace310b56838d8129c5dcc7
Doozy <tiagohok@gmail.com>
2018-10-20 02:00:09 +0700

    removed button hidden inputs for better compatibility

--- templates/ti-addtowishlist.php
+++ templates/ti-addtowishlist.php
@@ -2,7 +2,7 @@
 /**
  * The Template for displaying add to wishlist product button.
  *
- * @version             1.8.8
+ * @version             1.9.1
  * @package           TInvWishlist\Template
  */
 
@@ -12,21 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 wp_enqueue_script( 'tinvwl' );
 ?>
 <div class="tinv-wraper woocommerce tinv-wishlist <?php echo esc_attr( $class_postion ) ?>">
-	<input type="hidden" name="product_id"
-	       value="<?php echo esc_attr( ( version_compare( WC_VERSION, '3.0.0', '<' ) ? $product->get_id() : ( $product->is_type( 'variation' ) ? $product->get_parent_id() : $product->get_id() ) ) ); ?>"/>
-	<?php if ( $variation_id ) { ?>
-		<input type="hidden" name="variation_id" value="<?php echo esc_attr( $variation_id ); ?>"/>
-		<?php
-
-		$_variation = wc_get_product( $variation_id );
-		$attributes = $_variation->get_variation_attributes();
-
-		foreach ( $attributes as $name => $value ) {
-			echo '<input type="hidden" name="' . $name . '" value="' . $value . '" />';// WPCS: XSS ok.
-		}
-
-		?>
-	<?php } ?>
 	<?php do_action( 'tinv_wishlist_addtowishlist_button' ); ?>
 	<?php do_action( 'tinv_wishlist_addtowishlist_dialogbox' ); ?>
 	<div

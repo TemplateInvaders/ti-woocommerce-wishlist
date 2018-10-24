93b45b3a8aee28f942bebb1460d31f002057e1a4
Doozy <tiagohok@gmail.com>
2018-10-24 07:26:11 +0700

    WooCommerce version bumped to 3.5

--- ti-woocommerce-wishlist.php
+++ ti-woocommerce-wishlist.php
@@ -8,7 +8,7 @@
  * Requires at least: 4.5
  * Tested up to: 4.9
  * WC requires at least: 2.6
- * WC tested up to: 3.4
+ * WC tested up to: 3.5
  * Author:            TemplateInvaders
  * Author URI:        https://templateinvaders.com/
  * License:           GPL-2.0+

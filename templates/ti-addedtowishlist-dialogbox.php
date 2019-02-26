08805d1db6df2c3548d8943ea79bd1c313e79e7d
Doozy <tiagohok@gmail.com>
2019-02-26 16:58:22 +0700

    modal buttons order changed

--- templates/ti-addedtowishlist-dialogbox.php
+++ templates/ti-addedtowishlist-dialogbox.php
@@ -2,7 +2,7 @@
 /**
  * The Template for displaying dialog for message added to wishlist product.
  *
- * @version             1.8.0
+ * @version             1.9.15
  * @package           TInvWishlist\Template
  */
 
@@ -19,8 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<i class="<?php echo esc_attr( $icon ); ?>"></i>
 				<div class="tinv-txt"><?php echo $msg; // WPCS: xss ok. ?></div>
 				<div class="tinvwl-buttons-group tinv-wishlist-clear">
-					<button class="button tinvwl_button_close" type="button"><i
-							class="ftinvwl ftinvwl-times"></i><?php esc_html_e( 'Close', 'ti-woocommerce-wishlist' ); ?></button>
 					<?php if ( isset( $wishlist_url ) ) : ?>
 						<button class="button tinvwl_button_view tinvwl-btn-onclick"
 						        data-url="<?php echo esc_url( $wishlist_url ); ?>" type="button"><i
@@ -30,6 +28,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<?php if ( isset( $dialog_custom_url ) && isset( $dialog_custom_html ) ) : ?>
 						<button class="button tinvwl_button_view tinvwl-btn-onclick" data-url="<?php echo esc_url( $dialog_custom_url ); ?>" type="button"><?php echo $dialog_custom_html; // WPCS: xss ok. ?></button>
 					<?php endif; ?>
+					<button class="button tinvwl_button_close" type="button"><i
+							class="ftinvwl ftinvwl-times"></i><?php esc_html_e( 'Close', 'ti-woocommerce-wishlist' ); ?></button>
 				</div>
 				<div class="tinv-wishlist-clear"></div>
 			</div>

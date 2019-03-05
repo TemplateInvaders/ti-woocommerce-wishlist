6a66348ba69c0a1b364b2945ec90d3394be0e176
Doozy <tiagohok@gmail.com>
2019-03-05 07:10:28 +0700

    Added compatibility with [Woocommerce Product Addons](https://wordpress.org/plugins/woo-custom-product-addons/) plugin

--- templates/ti-wishlist-item-data.php
+++ templates/ti-wishlist-item-data.php
@@ -2,7 +2,7 @@
 /**
  * The Template for displaying variation product data.
  *
- * @version             1.0.0
+ * @version             1.9.15
  * @package           TInvWishlist\Template
  */
 
@@ -11,10 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="variation">
+<dl class="variation">
 	<?php foreach ( $item_data as $data ) : ?>
-	<span class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( $data['key'] ); ?> - </span>
-		<span class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( $data['display'] ); ?></span>
-		<br>
+		<dt class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( $data['key'] ); ?>
+			:
+		</dt>
+		<dd class="variation-<?php echo sanitize_html_class( $data['key'] ); ?>"><?php echo wp_kses_post( $data['display'] ); ?></dd>
+
 	<?php endforeach; ?>
-</div>
+</dl>

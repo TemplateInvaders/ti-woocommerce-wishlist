58116df8d8908ae70d535162d6993852945599fc
Doozy <tiagohok@gmail.com>
2019-08-13 02:27:24 +0700

    Added compatibility with Variations Swatches and Photos  plugin

---
+++ integrations/woocommerce-variation-swatches-and-photos.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * TI WooCommerce Wishlist integration with:
+ *
+ * @name WooCommerce Variation Swatches and Photos
+ *
+ * @version 3.0.12
+ *
+ * @slug woocommerce-variation-swatches-and-photos
+ *
+ * @url https://woocommerce.com/products/variation-swatches-and-photos/
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}

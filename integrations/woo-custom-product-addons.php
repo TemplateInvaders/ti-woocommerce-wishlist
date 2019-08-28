cf22536ad99a6b407204db304fc250d363785a82
Doozy <tiagohok@gmail.com>
2019-08-28 20:36:41 +0700

    improved integration with WooCommerce Custom Product Addons plugin

--- integrations/woo-custom-product-addons.php
+++ integrations/woo-custom-product-addons.php
@@ -36,6 +36,10 @@ if ( ! function_exists( 'tinv_wishlist_item_meta_woocommerce_custom_product_addo
 			$data     = array();
 			$post_ids = $form->get_form_ids( $product_id );
 
+			if ( isset( $item_data['wcpa_field_key_checker'] ) ) {
+				unset( $item_data['wcpa_field_key_checker'] );
+			}
+
 			if ( wcpa_get_option( 'form_loading_order_by_date' ) === true ) {
 				if ( is_array( $post_ids ) && count( $post_ids ) ) {
 					$post_ids = get_posts( array(
@@ -86,7 +90,7 @@ if ( ! function_exists( 'tinv_wishlist_item_meta_woocommerce_custom_product_addo
 							'name'      => $v->name,
 							'label'     => ( isset( $v->label ) ) ? $v->label : '',
 							'value'     => $value['display'],
-							'price'     => false,
+							'price'     => ( isset( $v->price ) ) ? $v->price : false,
 							'form_data' => $form_data,
 						), wc_get_product( $product_id ) );
 					}
@@ -99,3 +103,79 @@ if ( ! function_exists( 'tinv_wishlist_item_meta_woocommerce_custom_product_addo
 
 	add_filter( 'tinvwl_wishlist_item_meta_post', 'tinv_wishlist_item_meta_woocommerce_custom_product_addons', 10, 3 );
 }
+
+if ( ! function_exists( 'tinvwl_item_price_woocommerce_custom_product_addons' ) ) {
+
+	/**
+	 * Modify price for  WooCommerce Custom Product Addons.
+	 *
+	 * @param string $price Returned price.
+	 * @param array $wl_product Wishlist Product.
+	 * @param \WC_Product $product Woocommerce Product.
+	 *
+	 * @return string
+	 */
+	function tinvwl_item_price_woocommerce_custom_product_addons( $price, $wl_product, $product ) {
+		if ( defined( 'WCPA_ITEM_ID' ) ) {
+
+			$price    = 0;
+			$form     = new WCPA_Form();
+			$data     = array();
+			$post_ids = $form->get_form_ids( $wl_product['product_id'] );
+
+			if ( wcpa_get_option( 'form_loading_order_by_date' ) === true ) {
+				if ( is_array( $post_ids ) && count( $post_ids ) ) {
+					$post_ids = get_posts( array(
+						'posts_per_page' => - 1,
+						'include'        => $post_ids,
+						'fields'         => 'ids',
+						'post_type'      => WCPA_POST_TYPE,
+						'posts_per_page' => - 1,
+					) );
+				}
+			}
+			foreach ( $post_ids as $id ) {
+				if ( get_post_status( $id ) == 'publish' ) {
+					$json_string  = get_post_meta( $id, WCPA_FORM_META_KEY, true );
+					$json_encoded = json_decode( $json_string );
+					if ( $json_encoded && is_array( $json_encoded ) ) {
+						$data = array_merge( $data, $json_encoded );
+					}
+				}
+			}
+
+			foreach ( $data as $v ) {
+				$form_data = clone $v;
+				unset( $form_data->values ); //avoid saving large number of data
+				unset( $form_data->className ); //avoid saving no use data
+				if ( ! in_array( $v->type, array( 'header', 'paragraph' ) ) ) {
+					if ( isset( $wl_product['meta'][ $v->name ] ) ) {
+						if ( ! is_object( $v ) ) {
+							continue;
+						} else if ( ( isset( $v->name ) ) ) {
+							if ( ( ! isset( $v->is_fee ) || $v->is_fee === false ) && ( ! isset( $v->is_show_price ) || $v->is_show_price === false ) ) {
+								if ( isset( $v->price ) && is_array( $v->price ) ) {
+									foreach ( $v->price as $p ) {
+										$price += $p;
+									}
+								} else if ( isset( $v->price ) && $v->price ) {
+									$price += $v->price;
+								}
+							}
+						}
+					}
+				}
+			}
+
+			$mc    = new WCPA_MC();
+			$price = $mc->mayBeConvert( $price ) + $product->get_price( 'edit' );
+
+			$price = wc_price( $price );
+
+		}
+
+		return $price;
+	}
+
+	add_filter( 'tinvwl_wishlist_item_price', 'tinvwl_item_price_woocommerce_custom_product_addons', 10, 3 );
+} // End if().

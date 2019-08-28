e56aac787dfc77e3191ff1a9f275d89f3d272e85
Doozy <tiagohok@gmail.com>
2019-08-28 22:40:23 +0700

    Added integration with WooCommerce Show Single Variations by Iconic plugin

---
+++ integrations/show-single-variations.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TI WooCommerce Wishlist integration with:
+ *
+ * @name WooCommerce Show Single Variations by Iconic
+ *
+ * @version https://iconicwp.com
+ *
+ * @slug show-single-variations-premium
+ *
+ * @url https://iconicwp.com/products/woocommerce-show-single-variations/
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( class_exists( 'Iconic_WSSV_Product_Variation' ) && ! function_exists( 'tinvwl_product_visibility_fix' ) ) {
+
+	add_action( 'tinvwl_wishlist_contents_before', 'tinvwl_product_visibility_fix' );
+
+	function tinvwl_product_visibility_fix() {
+		add_filter( 'woocommerce_product_is_visible', '__return_true', 100, 2 );
+	}
+}

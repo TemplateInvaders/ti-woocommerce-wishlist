deeeaf5566eee90ac68224d97c1a6ac1e3c6a8e9
Doozy <tiagohok@gmail.com>
2019-12-12 06:52:21 +0700

    Enhanced compatibility with Improved Product Options for WooCommerce plugin

--- integrations/improved-variable-product-attributes.php
+++ integrations/improved-variable-product-attributes.php
@@ -4,7 +4,7 @@
  *
  * @name Improved Product Options for WooCommerce
  *
- * @version 4.8.0
+ * @version 4.9.3
  *
  * @slug improved-variable-product-attributes
  *
@@ -40,6 +40,7 @@ if ( ! function_exists( 'tinv_wishlist_meta_support_ivpa' ) ) {
 				$prefix  = 'attribute_';
 				$k_ivpac = ( 0 === strpos( $k, $prefix ) ) ? substr( $k, strlen( $prefix ) ) : $k_ivpac;
 				$v       = is_array( $v['display'] ) ? implode( ', ', $v['display'] ) : $v['display'];
+
 				if ( isset( $curr_customizations['ivpa_attr'][ $k_ivpac ] ) ) {
 					if ( $curr_customizations['ivpa_attr'][ $k_ivpac ] == 'ivpa_custom' ) {
 						$meta[ $k ] = array(
@@ -48,8 +49,21 @@ if ( ! function_exists( 'tinv_wishlist_meta_support_ivpa' ) ) {
 						);
 					}
 				}
+
 				if ( in_array( $k_ivpac, $curr_customizations['ivpa_attr'] ) ) {
-					if ( $product->is_type( 'variation' ) && $product->get_attribute( $k_ivpac ) === $v ) {
+
+					$attributes = $product->get_attributes();
+					$attribute  = sanitize_title( $k_ivpac );
+
+					$term_slug = '';
+
+					if ( isset( $attributes[ $attribute ] ) ) {
+						$term_slug = $attributes[ $attribute ];
+					} elseif ( isset( $attributes[ 'pa_' . $attribute ] ) ) {
+						$term_slug = $attributes[ 'pa_' . $attribute ];
+					}
+
+					if ( $product->is_type( 'variation' ) && $term_slug === $v ) {
 						unset( $meta[ $k ] );
 					} else {
 						$meta[ $k ] = array(
@@ -72,7 +86,7 @@ function tinv_add_to_wishlist_ivpa() {
 	if ( class_exists( 'XforWC_Improved_Options' ) ) {
 
 		wp_add_inline_script( 'tinvwl', "
-		jQuery(document).ready(function($){		
+		jQuery(document).ready(function($){
 		    $(document).on('tinvwl_wishlist_button_clicked', function (e, el, data) {
 				if (!ivpa) {
 					return false;
@@ -82,50 +96,54 @@ function tinv_add_to_wishlist_ivpa() {
 				var find = button.closest('.summary').length > 0 ? '#ivpa-content' : '.ivpa-content';
 
 				if ( container.find(find).length > 0 ) {
-						var var_id = container.find(find).attr('data-selected');
-			
-						if ( typeof var_id == 'undefined' || var_id == '' ) {
-							var_id = container.find('[name=\"variation_id\"]').val();
-						}
-			
-						if ( typeof var_id == 'undefined' || var_id == '' ) {
-							var_id = container.find(find).attr('data-id');
-						}
-			
-						var item = {};
-			
-						container.find(find+' .ivpa_attribute').each( function() {
-							var attribute = $(this).attr('data-attribute');
-							var attribute_value = $(this).find('.ivpa_term.ivpa_clicked').attr('data-term');
-			
-						data.form['attribute_' + attribute] = attribute_value;
-						});
-			
-						var ivpac = container.find(find+' .ivpa_custom_option').length>0 ? container.find(find+' .ivpa_custom_option [name^=\"ivpac_\"]').serialize() : '';
-			
+					var var_id = container.find(find).attr('data-selected');
+
+					if ( typeof var_id == 'undefined' || var_id == '' ) {
+						var_id = container.find('[name=\"variation_id\"]').val();
+					}
+
+					if ( typeof var_id == 'undefined' || var_id == '' ) {
+						var_id = container.find(find).attr('data-id');
+					}
+
+					var item = {};
+
+					container.find(find+' .ivpa_attribute').each( function() {
+						var attribute = $(this).attr('data-attribute');
+						var attribute_value = $(this).find('.ivpa_term.ivpa_clicked').attr('data-term');
+
+					data.form['attribute_' + attribute] = attribute_value;
+					});
+
+					var ivpac = container.find(find+' .ivpa_custom_option').length>0 ? container.find(find+' .ivpa_custom_option [name^=\"ivpac_\"]').serialize() : '';
+
 					var ivpac_fields = container.find(find + ' .ivpa_custom_option').length > 0 ? container.find(find + ' .ivpa_custom_option [name^=\"ivpac_\"]') : '';
-	
-					ivpac_fields.each(function () {
-	
-						var name = $(this).attr('name').replace(/\[.*\]/g, '');
-	
-						if ($(this).is(':checkbox')) {
-	
-							if (!$(this).is(':checked')) return true;
-	
-							if (data.form.hasOwnProperty(name) && data.form[name].length) {
-								data.form[name] = (data.form[name] + ', ' + $(this).val()).replace(/^, /, '');
+
+					if(ivpac_fields){
+
+						ivpac_fields.each(function () {
+
+							var name = $(this).attr('name').replace(/\[.*\]/g, '');
+
+							if ($(this).is(':checkbox')) {
+
+								if (!$(this).is(':checked')) return true;
+
+								if (data.form.hasOwnProperty(name) && data.form[name].length) {
+									data.form[name] = (data.form[name] + ', ' + $(this).val()).replace(/^, /, '');
+								} else {
+									data.form[name] = $(this).val();
+								}
 							} else {
 								data.form[name] = $(this).val();
 							}
-						} else {
-							data.form[name] = $(this).val();
-						}
-					});
-			
-						data.form.variation_id = var_id;						
-						data.ivpac = ivpac;
+						});
 					}
+
+					data.form.variation_id = var_id;
+					data.product_variation = var_id;
+					data.ivpac = ivpac;
+				}
 			});
         });
         " );

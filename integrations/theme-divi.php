9b9a7f93a67e80e2dad0db1d9ed5ccdc7cdff629
Doozy <tiagohok@gmail.com>
2019-12-12 06:51:36 +0700

    Added compatibility for Divi builder related to 'out of stock' issue

---
+++ integrations/theme-divi.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * TI WooCommerce Wishlist integration with:
+ *
+ * @name Divi
+ *
+ * @version 4.0.7
+ *
+ * @slug Divi
+ *
+ * @url http://www.elegantthemes.com/gallery/divi/
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+
+/**
+ * Run hooks on page redirect.
+ */
+function divi_init() {
+	if ( class_exists( 'ET_Builder_Element' ) && is_product() ) {
+		global $post;
+		$product = wc_get_product( $post->ID );
+		if ( ! empty( $product ) && ! $product->is_in_stock() ) {
+			add_action( 'woocommerce_' . $product->get_type() . '_add_to_cart', 'divi_single_product_summary', 40 );
+		}
+	}
+}
+
+add_action( 'template_redirect', 'divi_init' );
+
+// Add a custom hook for single page.
+function divi_single_product_summary() {
+	do_action( 'tinvwl_single_product_summary' );
+}

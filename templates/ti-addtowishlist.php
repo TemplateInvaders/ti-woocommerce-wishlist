aaf28824ca7719951a476a3580ca1f69cdbc4d6e
Doozy <tiagohok@gmail.com>
2020-06-25 21:23:58 +0700

    Added integration with WPC Product Bundles for WooCommerce plugin

--- templates/ti-addtowishlist.php
+++ templates/ti-addtowishlist.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 wp_enqueue_script( 'tinvwl' );
 ?>
 <div class="tinv-wraper woocommerce tinv-wishlist <?php echo esc_attr( $class_postion ) ?>">
-	<?php do_action( 'tinvwl_wishlist_addtowishlist_button' ); ?>
+	<?php do_action( 'tinvwl_wishlist_addtowishlist_button', $product, $loop ); ?>
 	<?php do_action( 'tinvwl_wishlist_addtowishlist_dialogbox' ); ?>
 	<div class="tinvwl-tooltip"><?php echo esc_html( tinv_get_option( 'add_to_wishlist' . ( $loop ? '_catalog' : '' ), 'text' ) ); ?></div>
 </div>

fd0cb0de213f11c9b7766500c1ba6119070a9df8
Doozy <tiagohok@gmail.com>
2020-09-08 15:06:59 +0700

    Fixed translation for template strings

--- templates/ti-addedtowishlist-dialogbox.php
+++ templates/ti-addedtowishlist-dialogbox.php
@@ -2,7 +2,7 @@
 /**
  * The Template for displaying dialog for message added to wishlist product.
  *
- * @version             1.21.5
+ * @version             1.21.6
  * @package           TInvWishlist\Template
  */
 
@@ -21,15 +21,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<div class="tinvwl-buttons-group tinv-wishlist-clear">
 					<?php if ( isset( $wishlist_url ) ) : ?>
 						<button class="button tinvwl_button_view tinvwl-btn-onclick"
-						        data-url="<?php echo esc_url( $wishlist_url ); ?>" type="button"><i
-								class="ftinvwl ftinvwl-heart-o"></i><?php echo wp_kses_post( apply_filters( 'tinvwl_view_wishlist_text', tinv_get_option( 'general', 'text_browse' ) ) ); ?>
+								data-url="<?php echo esc_url( $wishlist_url ); ?>" type="button"><i
+									class="ftinvwl ftinvwl-heart-o"></i><?php echo wp_kses_post( apply_filters( 'tinvwl_view_wishlist_text', tinv_get_option( 'general', 'text_browse' ) ) ); ?>
 						</button>
 					<?php endif; ?>
 					<?php if ( isset( $dialog_custom_url ) && isset( $dialog_custom_html ) ) : ?>
-						<button class="button tinvwl_button_view tinvwl-btn-onclick" data-url="<?php echo esc_url( $dialog_custom_url ); ?>" type="button"><?php echo $dialog_custom_html; // WPCS: xss ok. ?></button>
+						<button class="button tinvwl_button_view tinvwl-btn-onclick"
+								data-url="<?php echo esc_url( $dialog_custom_url ); ?>"
+								type="button"><?php echo $dialog_custom_html; // WPCS: xss ok. ?></button>
 					<?php endif; ?>
 					<button class="button tinvwl_button_close" type="button"><i
-							class="ftinvwl ftinvwl-times"></i><?php echo wp_kses_post( 'Close', 'ti-woocommerce-wishlist' ); ?></button>
+								class="ftinvwl ftinvwl-times"></i><?php esc_html_e( 'Close', 'ti-woocommerce-wishlist' ); ?>
+					</button>
 				</div>
 				<div class="tinv-wishlist-clear"></div>
 			</div>

4c600849f9ca854000c8c3cf43cf41e11f6acc8c
Doozy <tiagohok@gmail.com>
2021-02-01 17:36:55 +0700

    Added support of WooCommerce 4.9.x

--- ti-woocommerce-wishlist.php
+++ ti-woocommerce-wishlist.php
@@ -8,7 +8,7 @@
  * Requires at least: 4.7
  * Tested up to: 5.6
  * WC requires at least: 3.0
- * WC tested up to: 4.7
+ * WC tested up to: 4.9
  * Author:            TemplateInvaders
  * Author URI:        https://templateinvaders.com/
  * License:           GPL-2.0+

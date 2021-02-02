75889ebfa4e793f6199c83da39cc55969b59a69a
Doozy <tiagohok@gmail.com>
2021-02-02 14:48:33 +0700

    Fixed PHP warning in customizer for Flatsome theme integration

--- integrations/theme-flatsome.php
+++ integrations/theme-flatsome.php
@@ -13,60 +13,57 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
-add_action('after_setup_theme', 'tinvwl_flatsome');
+add_action( 'after_setup_theme', 'tinvwl_flatsome' );
 
-function tinvwl_flatsome()
-{
-	if ( ! class_exists('Flatsome_Default')) {
+function tinvwl_flatsome() {
+	if ( ! class_exists( 'Flatsome_Default' ) ) {
 		return;
 	}
 
 	// Catalog mode
-	if ( ! function_exists('tinvwl_flatsome_init')) {
+	if ( ! function_exists( 'tinvwl_flatsome_init' ) ) {
 
 		/**
 		 * Run hooks after theme init.
 		 */
-		function tinvwl_flatsome_init()
-		{
+		function tinvwl_flatsome_init() {
 
-			if (get_theme_mod('catalog_mode')) {
+			if ( get_theme_mod( 'catalog_mode' ) ) {
 
-				add_filter('tinvwl_allow_addtowishlist_single_product_summary', 'tinvwl_flatsome_woocommerce_catalog_mode', 10, 2);
+				add_filter( 'tinvwl_allow_addtowishlist_single_product_summary', 'tinvwl_flatsome_woocommerce_catalog_mode', 10, 2 );
 
-				switch (tinv_get_option('add_to_wishlist', 'position')) {
+				switch ( tinv_get_option( 'add_to_wishlist', 'position' ) ) {
 					case 'before':
-						add_action('woocommerce_single_variation', 'tinvwl_view_addto_html', 10);
+						add_action( 'woocommerce_single_variation', 'tinvwl_view_addto_html', 10 );
 						break;
 					case 'after':
-						add_action('woocommerce_single_variation', 'tinvwl_view_addto_html', 20);
+						add_action( 'woocommerce_single_variation', 'tinvwl_view_addto_html', 20 );
 						break;
 				}
 
-				add_action('woocommerce_single_variation', 'tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable', 20);
+				add_action( 'woocommerce_single_variation', 'tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable', 20 );
 
 			}
 		}
 
-		add_action('init', 'tinvwl_flatsome_init');
+		add_action( 'init', 'tinvwl_flatsome_init' );
 	}
 
-	if ( ! function_exists('tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable')) {
+	if ( ! function_exists( 'tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable' ) ) {
 
 		/**
 		 * Output variation hidden field.
 		 *
 		 */
-		function tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable()
-		{
+		function tinvwl_tinvwl_flatsome_woocommerce_catalog_mode_variable() {
 			echo '<input type="hidden" name="variation_id" class="variation_id" value="0" />';
 		}
 	}
 
-	if ( ! function_exists('tinvwl_flatsome_woocommerce_catalog_mode')) {
+	if ( ! function_exists( 'tinvwl_flatsome_woocommerce_catalog_mode' ) ) {
 
 		/**
 		 * Output wishlist button for Flatsome catalog mode
@@ -75,9 +72,8 @@ function tinvwl_flatsome()
 		 *
 		 * @return bool
 		 */
-		function tinvwl_flatsome_woocommerce_catalog_mode($allow, $product)
-		{
-			if ( ! $product->is_type('variable')) {
+		function tinvwl_flatsome_woocommerce_catalog_mode( $allow, $product ) {
+			if ( ! $product->is_type( 'variable' ) ) {
 				return true;
 			}
 
@@ -86,7 +82,7 @@ function tinvwl_flatsome()
 	}
 
 	// Header wishlist counter
-	if ( ! function_exists('tinvwl_flatsome_header_wishlist')) {
+	if ( ! function_exists( 'tinvwl_flatsome_header_wishlist' ) ) {
 		/**
 		 * Header Wishlist element
 		 *
@@ -94,27 +90,25 @@ function tinvwl_flatsome()
 		 *
 		 * @return mixed
 		 */
-		function tinvwl_flatsome_header_wishlist($elements)
-		{
-			$elements['wishlist'] = __('Wishlist', 'ti-woocommerce-wishlist');
+		function tinvwl_flatsome_header_wishlist( $elements ) {
+			$elements['wishlist'] = __( 'Wishlist', 'ti-woocommerce-wishlist' );
 
 			return $elements;
 		}
 	}
-	add_filter('flatsome_header_element', 'tinvwl_flatsome_header_wishlist');
+	add_filter( 'flatsome_header_element', 'tinvwl_flatsome_header_wishlist' );
 
-	if ( ! function_exists('tinvwl_flatsome_refresh_wishlist_partials')) {
+	if ( ! function_exists( 'tinvwl_flatsome_refresh_wishlist_partials' ) ) {
 
-		function tinvwl_flatsome_refresh_wishlist_partials(WP_Customize_Manager $wp_customize)
-		{
+		function tinvwl_flatsome_refresh_wishlist_partials( WP_Customize_Manager $wp_customize ) {
 
 			// Abort if selective refresh is not available.
-			if ( ! isset($wp_customize->selective_refresh)) {
+			if ( ! isset( $wp_customize->selective_refresh ) ) {
 				return;
 			}
 
 
-			$wp_customize->selective_refresh->add_partial('header-wishlist', array(
+			$wp_customize->selective_refresh->add_partial( 'header-wishlist', array(
 					'selector'            => '.header-wishlist-icon',
 					'container_inclusive' => true,
 					'settings'            => array(
@@ -125,29 +119,29 @@ function tinvwl_flatsome()
 							'header_wishlist_label'
 					),
 					'render_callback'     => tinvwl_flatsome_render_header_wishlist(),
-			));
+			) );
 
 		}
 	}
-	add_action('customize_register', 'tinvwl_flatsome_refresh_wishlist_partials');
+	add_action( 'customize_register', 'tinvwl_flatsome_refresh_wishlist_partials' );
 
 
 	$transport = 'postMessage';
-	if ( ! isset($wp_customize->selective_refresh)) {
+	if ( ! isset( $wp_customize->selective_refresh ) ) {
 		$transport = 'refresh';
 	}
 
 	$image_url = get_template_directory_uri() . '/inc/admin/customizer/img/';
-	Flatsome_Option::add_section('header_wishlist', array(
-			'title'    => __('Wishlist', 'flatsome-admin'),
+	Flatsome_Option::add_section( 'header_wishlist', array(
+			'title'    => __( 'Wishlist', 'flatsome-admin' ),
 			'panel'    => 'header',
 			'priority' => 110,
-	));
+	) );
 
-	Flatsome_Option::add_field('option', array(
+	Flatsome_Option::add_field( 'option', array(
 			'type'      => 'select',
 			'settings'  => 'wishlist_icon',
-			'label'     => __('Wishlist Icon', 'flatsome-admin'),
+			'label'     => __( 'Wishlist Icon', 'flatsome-admin' ),
 			'transport' => $transport,
 			'section'   => 'header_wishlist',
 			'default'   => 'heart',
@@ -160,13 +154,13 @@ function tinvwl_flatsome()
 					"menu"         => "List",
 					"pen-alt-fill" => "Pen",
 			),
-	));
+	) );
 
 
-	Flatsome_Option::add_field('option', array(
+	Flatsome_Option::add_field( 'option', array(
 			'type'      => 'radio-image',
 			'settings'  => 'wishlist_icon_style',
-			'label'     => __('Wishlist Icon Style', 'flatsome-admin'),
+			'label'     => __( 'Wishlist Icon Style', 'flatsome-admin' ),
 			'section'   => 'header_wishlist',
 			'transport' => $transport,
 			'default'   => '',
@@ -177,101 +171,98 @@ function tinvwl_flatsome()
 					'fill-round'    => $image_url . 'icon-fill-round.svg',
 					'outline-round' => $image_url . 'icon-outline-round.svg',
 			),
-	));
+	) );
 
 
-	Flatsome_Option::add_field('option', array(
+	Flatsome_Option::add_field( 'option', array(
 			'type'      => 'checkbox',
 			'settings'  => 'wishlist_title',
-			'label'     => __('Show Wishlist Title', 'flatsome-admin'),
+			'label'     => __( 'Show Wishlist Title', 'flatsome-admin' ),
 		//'description' => __( 'This is the control description', 'flatsome-admin' ),
 		//'help'        => __( 'This is some extra help. You can use this to add some additional instructions for users. The main description should go in the "description" of the field, this is only to be used for help tips.', 'flatsome-admin' ),
 			'section'   => 'header_wishlist',
 			'transport' => $transport,
 			'default'   => 1,
-	));
+	) );
 
-	Flatsome_Option::add_field('option', array(
+	Flatsome_Option::add_field( 'option', array(
 			'type'      => 'text',
 			'settings'  => 'header_wishlist_label',
-			'label'     => __('Custom Title', 'flatsome-admin'),
+			'label'     => __( 'Custom Title', 'flatsome-admin' ),
 			'section'   => 'header_wishlist',
 			'transport' => $transport,
 			'default'   => '',
-	));
+	) );
 
 
-	function tinvwl_flatsome_render_header_wishlist()
-	{
-		$icon       = get_theme_mod('wishlist_icon', flatsome_defaults('wishlist_icon'));
-		$icon_style = get_theme_mod('wishlist_icon_style', flatsome_defaults('wishlist_icon_style'));
-
+	function tinvwl_flatsome_render_header_wishlist() {
+		$icon       = get_theme_mod( 'wishlist_icon', flatsome_defaults( 'wishlist_icon' ) );
+		$icon_style = get_theme_mod( 'wishlist_icon_style', flatsome_defaults( 'wishlist_icon_style' ) );
+		ob_start();
 		?>
 		<li class="header-wishlist-icon">
-			<?php if ($icon_style) { ?>
+			<?php if ( $icon_style ) { ?>
 			<div class="header-button"><?php } ?>
 				<a href="<?php echo tinv_url_wishlist_default(); ?>"
-				   class="wishlist-link <?php echo get_flatsome_icon_class($icon_style, 'small'); ?>">
-					<?php if (get_theme_mod('wishlist_title', flatsome_defaults('wishlist_title'))) { ?>
+				   class="wishlist-link <?php echo get_flatsome_icon_class( $icon_style, 'small' ); ?>">
+					<?php if ( get_theme_mod( 'wishlist_title', flatsome_defaults( 'wishlist_title' ) ) ) { ?>
 						<span class="hide-for-medium header-wishlist-title">
-						  <?php if (get_theme_mod('header_wishlist_label', flatsome_defaults('header_wishlist_label'))) {
-							  echo get_theme_mod('header_wishlist_label', flatsome_defaults('header_wishlist_label'));
+						  <?php if ( get_theme_mod( 'header_wishlist_label', flatsome_defaults( 'header_wishlist_label' ) ) ) {
+							  echo get_theme_mod( 'header_wishlist_label', flatsome_defaults( 'header_wishlist_label' ) );
 						  } else {
-							  _e('Wishlist', 'woocommerce');
+							  _e( 'Wishlist', 'woocommerce' );
 						  } ?>
 						</span>
 					<?php } ?>
-					<?php if ($icon) { ?>
+					<?php if ( $icon ) { ?>
 						<i class="wishlist-icon icon-<?php echo $icon; ?>"
-						   <?php if (TInvWL_Public_WishlistCounter::counter() > 0){ ?>data-icon-label="<?php echo TInvWL_Public_WishlistCounter::counter(); ?>" <?php } ?>>
+						   <?php if ( TInvWL_Public_WishlistCounter::counter() > 0 ){ ?>data-icon-label="<?php echo TInvWL_Public_WishlistCounter::counter(); ?>" <?php } ?>>
 						</i>
 					<?php } ?>
 				</a>
-				<?php if ($icon_style) { ?> </div> <?php } ?>
+				<?php if ( $icon_style ) { ?> </div> <?php } ?>
 		</li> <?php
+		return ob_get_clean();
 	}
 
-	add_action('flatsome_header_elements', 'tinvwl_flatsome_hook_header_element');
+	add_action( 'flatsome_header_elements', 'tinvwl_flatsome_hook_header_element' );
 
-	function tinvwl_flatsome_hook_header_element($value)
-	{
-		if ('wishlist' === $value) {
-			tinvwl_flatsome_render_header_wishlist();
+	function tinvwl_flatsome_hook_header_element( $value ) {
+		if ( 'wishlist' === $value ) {
+			echo tinvwl_flatsome_render_header_wishlist();
 		}
 
 	}
 
 	// Add to wishlist button
-	if ( ! function_exists('tinvwl_flatsome_product_wishlist_button')) {
+	if ( ! function_exists( 'tinvwl_flatsome_product_wishlist_button' ) ) {
 		/**
 		 * Add wishlist Button to Product Image
 		 */
-		function tinvwl_flatsome_product_wishlist_button()
-		{
-			$icon = get_theme_mod('wishlist_icon', 'heart');
-			if ( ! $icon) {
+		function tinvwl_flatsome_product_wishlist_button() {
+			$icon = get_theme_mod( 'wishlist_icon', 'heart' );
+			if ( ! $icon ) {
 				$icon = 'heart';
 			}
 			?>
 			<div class="wishlist-icon">
 				<button class="wishlist-button button is-outline circle icon"
-						aria-label="<?php echo __('Wishlist', 'flatsome'); ?>">
-					<?php echo get_flatsome_icon('icon-' . $icon); ?>
+						aria-label="<?php echo __( 'Wishlist', 'flatsome' ); ?>">
+					<?php echo get_flatsome_icon( 'icon-' . $icon ); ?>
 				</button>
 				<div class="wishlist-popup dark">
-					<?php echo do_shortcode('[ti_wishlists_addtowishlist loop="yes"]'); ?>
+					<?php echo do_shortcode( '[ti_wishlists_addtowishlist loop="yes"]' ); ?>
 				</div>
 			</div>
 			<?php
 		}
 	}
-	add_action('flatsome_product_image_tools_top', 'tinvwl_flatsome_product_wishlist_button', 2);
-	add_action('flatsome_product_box_tools_top', 'tinvwl_flatsome_product_wishlist_button', 2);
+	add_action( 'flatsome_product_image_tools_top', 'tinvwl_flatsome_product_wishlist_button', 2 );
+	add_action( 'flatsome_product_box_tools_top', 'tinvwl_flatsome_product_wishlist_button', 2 );
 
 
-	function tinv_add_to_wishlist_flatsome()
-	{
-		wp_add_inline_script('tinvwl', "
+	function tinv_add_to_wishlist_flatsome() {
+		wp_add_inline_script( 'tinvwl', "
 					jQuery(document).ready(function($){
 						 $('body').on('click', '.wishlist-button', function (e) {
 							$(this).addClass('loading');
@@ -287,8 +278,8 @@ function tinvwl_flatsome()
 					jQuery('body').on('tinvwl_wishlist_product_marked', function(e, el,status){
 						jQuery(el).closest('div.wishlist-icon').find('.wishlist-button').toggleClass('wishlist-added', status);
 					});
-			");
+			" );
 	}
 
-	add_action('wp_enqueue_scripts', 'tinv_add_to_wishlist_flatsome', 100, 1);
+	add_action( 'wp_enqueue_scripts', 'tinv_add_to_wishlist_flatsome', 100, 1 );
 }

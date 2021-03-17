045ecc6a3966931d089247f26ecdfdc8c0279206
Doozy <tiagohok@gmail.com>
2021-03-17 18:13:02 +0700

    Added integration with [WooCommerce Square](https://woocommerce.com/products/square/?aff=3955) plugin

---
+++ integrations/woocommerce-square.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * TI WooCommerce Wishlist integration with:
+ *
+ * @name WooCommerce Square
+ *
+ * @version 2.3.4
+ *
+ * @slug woocommerce-square
+ *
+ * @url https://woocommerce.com/products/square/
+ *
+ */
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+	die;
+}
+
+if (!function_exists('tinv_wishlist_metaprepare_woocommerce_square')) {
+
+	/**
+	 * Set description for meta WooCommerce Square
+	 *
+	 * @param array $meta Meta array.
+	 * @param integer $product_id Product ID.
+	 * @param integer $variation_id Product variation ID.
+	 *
+	 * @return array
+	 */
+	function tinv_wishlist_metaprepare_woocommerce_square($meta, $product_id, $variation_id)
+	{
+
+		foreach (array_keys($meta) as $key) {
+			if (strpos($key, 'nds-pmd') === 0) {
+				unset($meta[$key]);
+			}
+		}
+
+		return $meta;
+	}
+
+	add_filter('tinvwl_wishlist_item_meta_post', 'tinv_wishlist_metaprepare_woocommerce_square', 10, 3);
+}

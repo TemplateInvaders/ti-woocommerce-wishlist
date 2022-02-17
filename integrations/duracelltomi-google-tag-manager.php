b669dc72dea8124b56b8347fec38b137e4345c72
Doozy <tiagohok@gmail.com>
2022-02-17 23:35:19 +0700

    Updated integration with [Google Tag Manager for WordPress](https://wordpress.org/plugins/duracelltomi-google-tag-manager/) plugin

--- integrations/duracelltomi-google-tag-manager.php
+++ integrations/duracelltomi-google-tag-manager.php
@@ -13,7 +13,7 @@
  */
 
 // If this file is called directly, abort.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -24,44 +24,45 @@ $slug = "duracelltomi-google-tag-manager";
 
 $name = "Google Tag Manager for WordPress";
 
-$available = defined('GTM4WP_PATH');
+$available = defined( 'GTM4WP_PATH' );
 
 $tinvwl_integrations = is_array( $tinvwl_integrations ) ? $tinvwl_integrations : [];
 
-$tinvwl_integrations[$slug] = array(
-	'name' => $name,
+$tinvwl_integrations[ $slug ] = array(
+	'name'      => $name,
 	'available' => $available,
 );
 
-if (!tinv_get_option('integrations', $slug)) {
+if ( ! tinv_get_option( 'integrations', $slug ) ) {
 	return;
 }
 
-if (!$available) {
+if ( ! $available ) {
 	return;
 }
 
 // Google Tag Manager for WordPress compatibility.
-if (!function_exists('tinv_wishlist_metaprepare_gtm4wp')) {
+if ( ! function_exists( 'tinvwl_wishlist_item_meta_post_gtm4wp' ) ) {
 
 	/**
-	 * Prepare save meta for WooCommerce - Google Tag Manager for WordPress
+	 * Set description for meta for WooCommerce - Google Tag Manager for WordPress
 	 *
 	 * @param array $meta Meta array.
+	 * @param array $wl_product Wishlist Product.
+	 * @param \WC_Product $product Woocommerce Product.
 	 *
 	 * @return array
 	 */
-	function tinv_wishlist_metaprepare_gtm4wp($meta)
-	{
+	function tinvwl_wishlist_item_meta_post_gtm4wp( $item_data, $product_id, $variation_id ) {
 
-		foreach (array_keys($meta) as $key) {
-			if (strpos($key, 'gtm4wp_') === 0) {
-				unset($meta[$key]);
+		foreach ( array_keys( $item_data ) as $key ) {
+			if ( strpos( $key, 'gtm4wp_' ) === 0 ) {
+				unset( $item_data[ $key ] );
 			}
 		}
 
-		return $meta;
+		return $item_data;
 	}
 
-	add_filter('tinvwl_product_prepare_meta', 'tinv_wishlist_metaprepare_gtm4wp');
+	add_filter( 'tinvwl_wishlist_item_meta_post', 'tinvwl_wishlist_item_meta_post_gtm4wp', 10, 3 );
 }

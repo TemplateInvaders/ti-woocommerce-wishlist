2b4583003c5f1ea59282f89e8412d2e17202424d
Doozy <tiagohok@gmail.com>
2022-09-09 16:01:45 +0700

    Intercom chat integration updated

--- admin/tinvwl.class.php
+++ admin/tinvwl.class.php
@@ -258,7 +258,7 @@ class TInvWL_Admin_TInvWL extends TInvWL_Admin_Base {
 				$parent_theme = $current_theme->parent();
 
 				wp_add_inline_script( $this->_name, 'window.intercomSettings = {
-					app_id: "wj7rirzi",
+					app_id: "zyh6v0pc",
 					"Website": "' . get_site_url() . '",
 					"Plugin name": "WooCommerce Wishlist Plugin",
 					"Plugin version":"' . TINVWL_FVERSION . '",

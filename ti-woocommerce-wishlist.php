021b554e297886f667ab478b5de6625e8fce3c71
Doozy <tiagohok@gmail.com>
2022-12-20 00:19:38 +0700

    WooCommerce 7.2 support

--- ti-woocommerce-wishlist.php
+++ ti-woocommerce-wishlist.php
@@ -8,7 +8,7 @@
  * Requires at least: 4.7
  * Tested up to: 6.1
  * WC requires at least: 3.0
- * WC tested up to: 7.1
+ * WC tested up to: 7.2
  * Author:            TemplateInvaders
  * Author URI:        https://templateinvaders.com/
  * License:           GPL-2.0+

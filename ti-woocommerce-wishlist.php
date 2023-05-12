62d6665822464142e9314d16f6336dfb2b840c82
Doozy <tiagohok@gmail.com>
2023-05-12 20:21:08 +0700

    Added support for WooCommerce 7.7

--- ti-woocommerce-wishlist.php
+++ ti-woocommerce-wishlist.php
@@ -8,7 +8,7 @@
  * Requires at least: 4.7
  * Tested up to: 6.2
  * WC requires at least: 3.0
- * WC tested up to: 7.6
+ * WC tested up to: 7.7
  * Author:            TemplateInvaders
  * Author URI:        https://templateinvaders.com/
  * License:           GPL-2.0+

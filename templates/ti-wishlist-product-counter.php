af07d16522a854783a4446bd332022b1372da7fe
Doozy <tiagohok@gmail.com>
2023-11-25 00:44:21 +0700

    Fixed issue with alt attribute of custom image for products counter icon

--- templates/ti-wishlist-product-counter.php
+++ templates/ti-wishlist-product-counter.php
@@ -4,7 +4,7 @@
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/ti-wishlist-product-counter.php.
  *
- * @version             2.3.1
+ * @version             2.8.0
  * @package           TInvWishlist\Template
  */
 
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 wp_enqueue_script( 'tinvwl' );
 if ( $icon_class && 'custom' === $icon && ! empty( $icon_upload ) ) {
-	$text = sprintf( '<img src="%s" /> %s', esc_url( $icon_upload ), $text );
+	$text = sprintf( '<img alt="%s" src="%s" /> %s', apply_filters( 'tinvwl_default_wishlist_title', tinv_get_option( 'general', 'default_title' ) ), esc_url( $icon_upload ), $text );
 }
 ?>
 <a href="<?php echo esc_url( tinv_url_wishlist_default() ); ?>"

2d96f1a8dfaaddc288bb37958f3fee22d8a77f3f
Doozy <tiagohok@gmail.com>
2025-06-05 01:20:15 +0700

    - updated integration with PW WooCommerce Gift Cards plugin

--- integrations/pw-woocommerce-gift-cards.php
+++ integrations/pw-woocommerce-gift-cards.php
@@ -4,7 +4,7 @@
  *
  * @name PW WooCommerce Gift Cards
  *
- * @version 1.298
+ * @version 2.14
  *
  * @slug pw-woocommerce-gift-cards
  *
@@ -13,7 +13,7 @@
  */
 
 // If this file is called directly, abort.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -24,26 +24,26 @@ $slug = "pw-woocommerce-gift-cards";
 
 $name = "PW WooCommerce Gift Cards";
 
-$available = defined('PWGC_VERSION');
+$available = defined( 'PWGC_VERSION' );
 
-$tinvwl_integrations = is_array($tinvwl_integrations) ? $tinvwl_integrations : [];
+$tinvwl_integrations = is_array( $tinvwl_integrations ) ? $tinvwl_integrations : [];
 
-$tinvwl_integrations[$slug] = array(
-	'name' => $name,
+$tinvwl_integrations[ $slug ] = array(
+	'name'      => $name,
 	'available' => $available,
 );
 
-if (!tinv_get_option('integrations', $slug)) {
+if ( ! tinv_get_option( 'integrations', $slug ) ) {
 	return;
 }
 
-if (!$available) {
+if ( ! $available ) {
 	return;
 }
 
-if (defined('PWGC_VERSION')) {
+if ( defined( 'PWGC_VERSION' ) ) {
 
-	if (!function_exists('tinv_wishlist_item_meta_pw_woocommerce_gift_cards')) {
+	if ( ! function_exists( 'tinv_wishlist_item_meta_pw_woocommerce_gift_cards' ) ) {
 
 		/**
 		 * Set description for meta  PW WooCommerce Gift Cards
@@ -54,15 +54,14 @@ if (defined('PWGC_VERSION')) {
 		 *
 		 * @return array
 		 */
-		function tinv_wishlist_item_meta_pw_woocommerce_gift_cards($item_data, $product_id, $variation_id)
-		{
+		function tinv_wishlist_item_meta_pw_woocommerce_gift_cards( $item_data, $product_id, $variation_id ) {
 
-			if (defined('PWGC_VERSION')) {
+			if ( defined( 'PWGC_VERSION' ) ) {
 				global $pw_gift_cards;
 
-				foreach ($pw_gift_cards->gift_card_meta as $key => $display) {
-					if (isset($item_data[$key])) {
-						$item_data[$key]['key'] = $display;
+				foreach ( $pw_gift_cards->gift_card_meta as $key => $display ) {
+					if ( isset( $item_data[ $key ] ) ) {
+						$item_data[ $key ]['key'] = $display;
 					}
 				}
 
@@ -71,10 +70,10 @@ if (defined('PWGC_VERSION')) {
 			return $item_data;
 		}
 
-		add_filter('tinvwl_wishlist_item_meta_post', 'tinv_wishlist_item_meta_pw_woocommerce_gift_cards', 10, 3);
+		add_filter( 'tinvwl_wishlist_item_meta_post', 'tinv_wishlist_item_meta_pw_woocommerce_gift_cards', 10, 3 );
 	} // End if().
 
-	if (!function_exists('tinvwl_item_price_pw_woocommerce_gift_cards')) {
+	if ( ! function_exists( 'tinvwl_item_price_pw_woocommerce_gift_cards' ) ) {
 
 		/**
 		 * Modify price for  PW WooCommerce Gift Cards.
@@ -85,15 +84,14 @@ if (defined('PWGC_VERSION')) {
 		 *
 		 * @return string
 		 */
-		function tinvwl_item_price_pw_woocommerce_gift_cards($price, $wl_product, $product)
-		{
+		function tinvwl_item_price_pw_woocommerce_gift_cards( $price, $wl_product, $product ) {
 
-			if (defined('PWGC_VERSION')) {
-				if ($product->get_type() == PWGC_PRODUCT_TYPE_SLUG) {
-					$id = ($wl_product['variation_id']) ? $wl_product['variation_id'] : $wl_product['product_id'];
-					$p = wc_get_product($id);
+			if ( defined( 'PWGC_VERSION' ) ) {
+				if ( $product->get_type() == PWGC_PRODUCT_TYPE_SLUG ) {
+					$id = ( $wl_product['variation_id'] ) ? $wl_product['variation_id'] : $wl_product['product_id'];
+					$p  = wc_get_product( $id );
 
-					if ($p) {
+					if ( $p ) {
 						return $p->get_price_html();
 					}
 				}
@@ -102,16 +100,15 @@ if (defined('PWGC_VERSION')) {
 			return $price;
 		}
 
-		add_filter('tinvwl_wishlist_item_price', 'tinvwl_item_price_pw_woocommerce_gift_cards', 10, 3);
+		add_filter( 'tinvwl_wishlist_item_price', 'tinvwl_item_price_pw_woocommerce_gift_cards', 10, 3 );
 	} // End if().
 
 
-	add_filter('tinvwl_addtowishlist_modify_type', 'tinvwl_addtowishlist_modify_type_pw_woocommerce_gift_cards', 10, 2);
+	add_filter( 'tinvwl_addtowishlist_modify_type', 'tinvwl_addtowishlist_modify_type_pw_woocommerce_gift_cards', 10, 2 );
 
-	function tinvwl_addtowishlist_modify_type_pw_woocommerce_gift_cards($type, $post)
-	{
-		if (defined('PWGC_VERSION')) {
-			if ($type == PWGC_PRODUCT_TYPE_SLUG) {
+	function tinvwl_addtowishlist_modify_type_pw_woocommerce_gift_cards( $type, $post ) {
+		if ( defined( 'PWGC_VERSION' ) ) {
+			if ( $type == PWGC_PRODUCT_TYPE_SLUG ) {
 				return 'variable';
 			}
 		}
@@ -120,9 +117,8 @@ if (defined('PWGC_VERSION')) {
 	}
 
 
-	function tinv_add_to_wishlist_pw_woocommerce_gift_cards()
-	{
-		wp_add_inline_script('tinvwl', "
+	function tinv_add_to_wishlist_pw_woocommerce_gift_cards() {
+		wp_add_inline_script( 'tinvwl', "
 					jQuery('body').on('tinvwl_add_to_wishlist_button_click', function(e, el){
 						if ('pw-gift-card' === jQuery(el).data('tinv-wl-producttype')){
 							jQuery(el).closest('form.cart').each(function(){
@@ -134,8 +130,8 @@ if (defined('PWGC_VERSION')) {
 							});
 						}
 					});
-			");
+			" );
 	}
 
-	add_action('wp_enqueue_scripts', 'tinv_add_to_wishlist_pw_woocommerce_gift_cards', 100, 1);
+	add_action( 'wp_enqueue_scripts', 'tinv_add_to_wishlist_pw_woocommerce_gift_cards', 100, 1 );
 }

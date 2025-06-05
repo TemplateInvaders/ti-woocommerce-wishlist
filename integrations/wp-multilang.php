37dca276c638840e8ffaa3e3946ca7bc61db0ff5
Doozy <tiagohok@gmail.com>
2025-06-05 13:59:47 +0700

    - updated integration with WP Multilang – Translation and Multilingual Plugin plugin

--- integrations/wp-multilang.php
+++ integrations/wp-multilang.php
@@ -4,7 +4,7 @@
  *
  * @name WP Multilang
  *
- * @version 2.3.0
+ * @version 2.4.19.1
  *
  * @slug wp-multilang
  *
@@ -13,7 +13,7 @@
  */
 
 // If this file is called directly, abort.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -24,43 +24,43 @@ $slug = "wp-multilang";
 
 $name = "WP Multilang";
 
-$available = function_exists('wpm_translate_string');
+$available = function_exists( 'wpm_translate_string' );
 
 $tinvwl_integrations = is_array( $tinvwl_integrations ) ? $tinvwl_integrations : [];
 
-$tinvwl_integrations[$slug] = array(
-	'name' => $name,
+$tinvwl_integrations[ $slug ] = array(
+	'name'      => $name,
 	'available' => $available,
 );
 
-if (!tinv_get_option('integrations', $slug)) {
+if ( ! tinv_get_option( 'integrations', $slug ) ) {
 	return;
 }
 
-if (!$available) {
+if ( ! $available ) {
 	return;
 }
 
 // WP Multilang string translations.
-if (function_exists('wpm_translate_string')) {
+if ( function_exists( 'wpm_translate_string' ) ) {
 
-	add_filter('tinvwl_default_wishlist_title', 'wpm_translate_string');
-	add_filter('tinvwl_view_wishlist_text', 'wpm_translate_string');
-	add_filter('tinvwl_added_to_wishlist_text', 'wpm_translate_string');
-	add_filter('tinvwl_already_in_wishlist_text', 'wpm_translate_string');
-	add_filter('tinvwl_removed_from_wishlist_text', 'wpm_translate_string');
-	add_filter('tinvwl_remove_from_wishlist_text', 'wpm_translate_string');
+	add_filter( 'tinvwl_default_wishlist_title', 'wpm_translate_string' );
+	add_filter( 'tinvwl_view_wishlist_text', 'wpm_translate_string' );
+	add_filter( 'tinvwl_added_to_wishlist_text', 'wpm_translate_string' );
+	add_filter( 'tinvwl_already_in_wishlist_text', 'wpm_translate_string' );
+	add_filter( 'tinvwl_removed_from_wishlist_text', 'wpm_translate_string' );
+	add_filter( 'tinvwl_remove_from_wishlist_text', 'wpm_translate_string' );
 
-	add_filter('tinvwl_added_to_wishlist_text_loop', 'wpm_translate_string');
-	add_filter('tinvwl_remove_from_wishlist_text_loop', 'wpm_translate_string');
+	add_filter( 'tinvwl_added_to_wishlist_text_loop', 'wpm_translate_string' );
+	add_filter( 'tinvwl_remove_from_wishlist_text_loop', 'wpm_translate_string' );
 
-	add_filter('tinvwl_add_to_cart_text', 'wpm_translate_string');
+	add_filter( 'tinvwl_add_to_cart_text', 'wpm_translate_string' );
 
-	add_filter('tinvwl_add_selected_to_cart_text', 'wpm_translate_string');
-	add_filter('tinvwl_add_all_to_cart_text', 'wpm_translate_string');
+	add_filter( 'tinvwl_add_selected_to_cart_text', 'wpm_translate_string' );
+	add_filter( 'tinvwl_add_all_to_cart_text', 'wpm_translate_string' );
 
-	add_filter('tinvwl_share_on_text', 'wpm_translate_string');
+	add_filter( 'tinvwl_share_on_text', 'wpm_translate_string' );
 
-	add_filter('tinvwl_wishlist_products_counter_text', 'wpm_translate_string');
+	add_filter( 'tinvwl_wishlist_products_counter_text', 'wpm_translate_string' );
 
 } // End if().

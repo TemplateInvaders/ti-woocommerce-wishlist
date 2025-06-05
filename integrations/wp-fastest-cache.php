0c0189426b670fa2d3dc921c2f0fa9a3bb928d5a
Doozy <tiagohok@gmail.com>
2025-06-05 13:58:35 +0700

    - updated integration with WP Fastest Cache plugin

--- integrations/wp-fastest-cache.php
+++ integrations/wp-fastest-cache.php
@@ -4,7 +4,7 @@
  *
  * @name WP Fastest Cache
  *
- * @version 0.9.1.0
+ * @version 1.3.7
  *
  * @slug wp-fastest-cache
  *
@@ -13,7 +13,7 @@
  */
 
 // If this file is called directly, abort.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -24,88 +24,87 @@ $slug = "wp-fastest-cache";
 
 $name = "WP Fastest Cache";
 
-$available = defined('WPFC_WP_PLUGIN_DIR');
+$available = defined( 'WPFC_WP_PLUGIN_DIR' );
 
 $tinvwl_integrations = is_array( $tinvwl_integrations ) ? $tinvwl_integrations : [];
 
-$tinvwl_integrations[$slug] = array(
-	'name' => $name,
+$tinvwl_integrations[ $slug ] = array(
+	'name'      => $name,
 	'available' => $available,
 );
 
-if (!tinv_get_option('integrations', $slug)) {
+if ( ! tinv_get_option( 'integrations', $slug ) ) {
 	return;
 }
 
-if (!$available) {
+if ( ! $available ) {
 	return;
 }
 
-if (!function_exists('tinvwl_wp_fastest_cache_reject')) {
+if ( ! function_exists( 'tinvwl_wp_fastest_cache_reject' ) ) {
 	/**
 	 * Disable cache for WP Fastest Cache
 	 */
-	function tinvwl_wp_fastest_cache_reject()
-	{
-		if (defined('WPFC_WP_PLUGIN_DIR')) {
-			$rules_json = get_option('WpFastestCacheExclude');
-			$ids = array(
-				tinv_get_option('page', 'wishlist'),
-				tinv_get_option('page', 'manage'),
+	function tinvwl_wp_fastest_cache_reject() {
+		if ( defined( 'WPFC_WP_PLUGIN_DIR' ) ) {
+			$rules_json = get_option( 'WpFastestCacheExclude' );
+			$ids        = array(
+				tinv_get_option( 'page', 'wishlist' ),
+				tinv_get_option( 'page', 'manage' ),
 			);
-			$pages = $ids;
-			$languages = apply_filters('wpml_active_languages', array(), array(
+			$pages      = $ids;
+			$languages  = apply_filters( 'wpml_active_languages', array(), array(
 				'skip_missing' => 0,
-				'orderby' => 'code',
-			));
-			if (!empty($languages)) {
-				foreach ($ids as $id) {
-					foreach ($languages as $l) {
-						$pages[] = apply_filters('wpml_object_id', $id, 'page', true, $l['language_code']);
+				'orderby'      => 'code',
+			) );
+			if ( ! empty( $languages ) ) {
+				foreach ( $ids as $id ) {
+					foreach ( $languages as $l ) {
+						$pages[] = apply_filters( 'wpml_object_id', $id, 'page', true, $l['language_code'] );
 					}
 				}
-				$pages = array_unique($pages);
+				$pages = array_unique( $pages );
 			}
-			$pages = array_filter($pages);
-			if (!empty($pages)) {
-				foreach ($pages as $i => $page) {
-					$pages[$i] = preg_replace("/^\//", '', str_replace(get_site_url(), '', get_permalink($page))); // @codingStandardsIgnoreLine Squiz.Strings.DoubleQuoteUsage.NotRequired
+			$pages = array_filter( $pages );
+			if ( ! empty( $pages ) ) {
+				foreach ( $pages as $i => $page ) {
+					$pages[ $i ] = preg_replace( "/^\//", '', str_replace( get_site_url(), '', get_permalink( $page ) ) ); // @codingStandardsIgnoreLine Squiz.Strings.DoubleQuoteUsage.NotRequired
 				}
 			}
-			$pages = array_unique($pages);
-			$pages = array_filter($pages);
+			$pages = array_unique( $pages );
+			$pages = array_filter( $pages );
 
-			$rules_std = json_decode($rules_json, true);
-			$ex_pages = array();
-			$rules_std = is_array($rules_std) ? $rules_std : array();
-			foreach ($rules_std as $value) {
-				$value['type'] = isset($value['type']) ? $value['type'] : 'page';
-				if ('page' === $value['type']) {
+			$rules_std = json_decode( $rules_json, true );
+			$ex_pages  = array();
+			$rules_std = is_array( $rules_std ) ? $rules_std : array();
+			foreach ( $rules_std as $value ) {
+				$value['type'] = isset( $value['type'] ) ? $value['type'] : 'page';
+				if ( 'page' === $value['type'] ) {
 					$ex_pages[] = $value['content'];
 				}
 			}
-			$ex_pages = array_unique($ex_pages);
-			$ex_pages = array_filter($ex_pages);
-			$changed = false;
+			$ex_pages = array_unique( $ex_pages );
+			$ex_pages = array_filter( $ex_pages );
+			$changed  = false;
 
-			foreach ($pages as $page) {
-				$page = preg_replace('/\/$/', '', $page);
+			foreach ( $pages as $page ) {
+				$page = preg_replace( '/\/$/', '', $page );
 
-				if (!in_array($page, $ex_pages)) {
-					$changed = true;
+				if ( ! in_array( $page, $ex_pages ) ) {
+					$changed     = true;
 					$rules_std[] = array(
-						'prefix' => 'startwith',
+						'prefix'  => 'startwith',
 						'content' => $page,
-						'type' => 'page',
+						'type'    => 'page',
 					);
 				}
 			}
-			if ($changed) {
-				$data = json_encode($rules_std);
-				update_option('WpFastestCacheExclude', $data);
+			if ( $changed ) {
+				$data = json_encode( $rules_std );
+				update_option( 'WpFastestCacheExclude', $data );
 			}
 		} // End if().
 	}
 
-	add_action('admin_init', 'tinvwl_wp_fastest_cache_reject');
+	add_action( 'admin_init', 'tinvwl_wp_fastest_cache_reject' );
 } // End if().
